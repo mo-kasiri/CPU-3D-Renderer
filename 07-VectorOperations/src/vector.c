21871e3a79d8609a2bde7ee4598cdf3d54dc73fb
mo-kasiri <mkgh7030@gmail.com>
2024-09-30 17:33:23 +0330

    add vector length, add

--- 07-VectorOperations/src/vector.c
+++ 07-VectorOperations/src/vector.c
@@ -1,11 +1,43 @@
 #include <math.h>
 #include "vector.h"
 
-// TODO: Implementation of all vector functions
+// 2D Vectors
 
-//////////////////////////////////
+//////////////////////////////////////////////
+// Vector Length
+//////////////////////////////////////////////
+float vec2_length(const vec2_t v)
+{
+    return (sqrt(pow(v.x,2)+ pow(v.y,2)));
+}
+vec2_t vec2_add(const vec2_t v, const vec2_t w){
+    vec2_t result = {
+        .x = v.x + w.x,
+        .y = v.y + w.y
+    };
+    return result;
+};
+
+
+// 3D vectors
+float vec3_length(const vec3_t v)
+{
+    return (sqrt(pow(v.x,2) + pow(v.y,2) + pow(v.z,2)));
+};
+
+vec3_t vec3_add(const vec3_t v, const vec3_t w){
+    vec3_t result = {
+        .x = v.x + w.x,
+        .y = v.y + w.y,
+        .z = v.z + w.z
+    };
+    return result;
+};
+
+
+///////////////////////////////////////////////
 /// Rotation Functions
-/////////////////////////////////
+///////////////////////////////////////////////
 vec3_t vec3_rotate_x(vec3_t v, float angle)
 {
     vec3_t rotated_vector = {
--- 07-VectorOperations/src/vector.h
+++ 07-VectorOperations/src/vector.h
@@ -7,6 +7,7 @@ typedef struct
     float y;
 } vec2_t;
 
+
 typedef struct
 {
     float x;
@@ -14,7 +15,23 @@ typedef struct
     float z;
 } vec3_t;
 
-// TODO: Add functions to manipulate vectors
+
+
+
+//////////////////////////////////////////////////////
+// Vector 2D functions 
+//////////////////////////////////////////////////////
+
+float vec2_length(const vec2_t v);
+vec2_t vec2_add(const vec2_t v, const vec2_t w);
+
+//////////////////////////////////////////////////////
+// Vector 3D functions 
+//////////////////////////////////////////////////////
+
+float vec3_length(const vec3_t v);
+vec3_t vec3_add(const vec3_t v, const vec3_t w);
+
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
